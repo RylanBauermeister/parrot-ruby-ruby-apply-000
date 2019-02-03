8d84272cedda4f17fe3f099cfa196b035b23f3b6
Rylan Bauermeister <rylan@bauermeister.com>
2019-02-03 04:55:57 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,11 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(phrase)
-  if phrase == nil
-    puts "Squawk!"
-    return "Squawk!"
-  end
-  
+def parrot(phrase = "Squawk!")
   puts phrase
   return phrase
 end
